647141fe9b96dc53b20b261151aca496677f1d40
Vishnu Gamini <gamini.vishnu@gmail.com>
2024-08-05 00:12:49 +0530

    updates

--- index.js
+++ index.js
@@ -26,36 +26,41 @@ io.on('connection', (socket) => {
         socket.join(user.room)
         socket.emit('message', generateMessage('Welcome!'))
         socket.broadcast.to(user.room).emit('message',generateMessage(`${user.username} has joined!`))
-        io.to(user.room).emit('roomData',{
-            room:user.room,
-            users:getUsersInRoom(user.room)
-        })
-        callback()
-    })
-
-    socket.on('sendMessage', (message,callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit('message',generateMessage(user.username,message))
-        callback()
-    })
-
-    socket.on('sendLocation',(coords,callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit('locationMessage',generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
-        callback()
-    })
-
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id)
-        if (user){
-            io.to(user.room).emit('message',generateMessage(`${user.username} has left!`))
+        if(user.room){
             io.to(user.room).emit('roomData',{
                 room:user.room,
                 users:getUsersInRoom(user.room)
             })
         }
 
-        
+        callback()
+    })
+
+    socket.on('sendMessage', (message,callback) => {
+        const user = getUser(socket.id)
+        if(user.room){
+            io.to(user.room).emit('message',generateMessage(user.username,message))
+        }
+        callback()
+    })
+
+    socket.on('sendLocation',(coords,callback) => {
+        const user = getUser(socket.id)
+        if (user.room){
+            io.to(user.room).emit('locationMessage',generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        }
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+        if (user && user.room){
+            io.to(user.room).emit('message',generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('roomData',{
+                room:user.room,
+                users:getUsersInRoom(user.room)
+            })
+        }
     })
 
 })
